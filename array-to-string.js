cedb724d2f37f7331c2ec36b3f6ed0ef0f602f15
marielacp <marielacp@outlook.com.pe>
2017-10-06 11:57:32 -0500

    1er ejercicio hours office

--- array-to-string.js
+++ array-to-string.js
@@ -2,4 +2,10 @@
 Crear una funcion que reciba como parametro un array y retorne una cadena
 -> retorne quack! sneeze! boom!
 */
-var arr = ["quack","sneeze","boom"];
+
+function stringLetter (array){
+a.push('');
+return a.join('! ');
+}
+console.log(stringLetter(a));
+var a= ["quack","sneeze","boom"];
